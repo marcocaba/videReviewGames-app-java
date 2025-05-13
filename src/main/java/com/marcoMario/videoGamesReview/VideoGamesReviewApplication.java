21dbd0a92e6dfae5b58b81a94297f36345933618
Mnq2610 <mario.nievaquintana@gmail.com>
2025-05-13 19:34:06 +0200

    Primer comit del Proyecto TFG

---
+++ src/main/java/com/marcoMario/videoGamesReview/VideoGamesReviewApplication.java
@@ -0,0 +1,13 @@
+package com.marcoMario.videoGamesReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoGamesReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoGamesReviewApplication.class, args);
+	}
+
+}
