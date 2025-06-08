9645b8a4b08d0ed7b22e947ff41e4fa21c66053c
Mnq2610 <mario.nievaquintana@gmail.com>
2025-06-08 17:06:15 +0200

    wip

--- src/main/java/com/marcoMario/restController/GameRestController.java
+++ src/main/java/com/marcoMario/restController/GameRestController.java
@@ -5,10 +5,7 @@ import com.marcoMario.model.DTO.GameDTO;
 import com.marcoMario.model.DTO.ObjectPage;
 import com.marcoMario.model.Game;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,8 +16,8 @@ public class GameRestController {
     private IGameService gameService;
 
     @CrossOrigin(origins = "http://localhost:4200")
-    @GetMapping("/getGameById")
-    public Game getGameById(@RequestParam("idGame") int idGame) {
+    @GetMapping("/getGameById/{idGame}")
+    public Game getGameById(@PathVariable("idGame") int idGame) {
         return gameService.getGameById(idGame);
     }
 
