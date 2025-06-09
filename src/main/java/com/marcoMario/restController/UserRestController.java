a1ab43d0a57c827dd1ff003fd079ccacfaa9a381
Mnq2610 <mario.nievaquintana@gmail.com>
2025-06-09 02:34:22 +0200

    user Services and controllers added

--- src/main/java/com/marcoMario/restController/UserRestController.java
+++ src/main/java/com/marcoMario/restController/UserRestController.java
@@ -2,9 +2,13 @@ package com.marcoMario.restController;
 
 import com.marcoMario.iService.IGameService;
 import com.marcoMario.iService.IUserService;
+import com.marcoMario.model.DTO.GameDTO;
+import com.marcoMario.model.DTO.ObjectPage;
 import com.marcoMario.model.Game;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UserRestController {
@@ -17,4 +21,23 @@ public class UserRestController {
     public boolean registerUser(@RequestParam String nameUser,@RequestParam String password ) {
         return userService.registerUser(nameUser,password);
     }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @PostMapping("/viewFavoriteGamesById/{page}")
+    public ObjectPage viewFavoriteGamesById(@RequestParam("idUser") long idUser, @PathVariable("page") int page ) {
+        return userService.viewFavoriteGamesById(idUser,page);
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @PostMapping("/addGameToFavorites")
+    public String addGameToFavorites(@RequestParam("idUser") long idUser, @RequestParam("gameId") long gameId ) {
+        return userService.addGameToFavorites(idUser,gameId);
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @PostMapping("/removeGameFromFavorites")
+    public String removeGameFromFavorites(@RequestParam("idUser") long idUser, @RequestParam("gameId") long gameId ) {
+        return userService.removeGameFromFavorites(idUser,gameId);
+    }
+
 }
